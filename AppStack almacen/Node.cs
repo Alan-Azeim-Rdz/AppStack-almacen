32c3c156376e47b17b265a553a48be1ff98d90fa
RODRIGUEZ VILLANUEVA ALAN AZEIM <I23050359@monclova.tecnm.mx>
2024-10-17 19:25:16 -0600

    Agregar archivos de proyecto.

---
+++ AppStack almacen/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppStack_almacen
+{
+    internal class Node
+    {
+        public int Datos { get; set; }
+        public Node Next { get; set; } // Apunta al siguiente nodo
+
+        public Node(int datos)
+        {
+            Datos = datos;
+            Next = null; // El siguiente nodo inicialmente es null
+        }
+    }
+}
---
+++ AppStack almacen/Stack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppStack_almacen
+{
+    internal class Stack
+    {
+        Node top;
+
+        public Stack() 
+        {
+            top = null;
+        }
+
+        // Método para insertar un elemento en la pila
+        public void Push(int dato)
+        {
+            Node newNode = new Node(dato);
+            if (IsEmpty())
+            {
+                top = newNode;
+            }
+            else
+            {
+                Node current = top;
+                newNode.Next = top;
+                top = newNode;
+            }
+        }
+
+
+        // Método para eliminar y devolver el elemento en el tope de la pila
+        public string Pop()
+        {
+            if (IsEmpty())
+            {
+                return("La pila está vacía.");
+            }
+
+            int dato = top.Datos;
+            top = top.Next;
+            return dato.ToString();
+        }
+
+
+        // Método para devolver el elemento en el tope de la pila sin eliminarlo
+        public string Peek()
+        {
+            if (IsEmpty())
+            {
+                return("La pila está vacía.");
+            }
+
+            return top.Datos.ToString();
+        }
+
+
+        // Método para verificar si la pila está vacía
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+
+
+
+
+    }
+}
